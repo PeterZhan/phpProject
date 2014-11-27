4fd4252e6ef5fac94745b6de4a4085194a88fa58
PeterZhan <hlzhangxt@126.com>
2014-11-27 13:13:06 -0500

    initialize

---
+++ application/controllers/login.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of login
+ *
+ * @author Administrator
+ */
+class Login extends CI_Controller {
+    //put your code here
+   
+ function __construct()
+ {
+   parent::__construct();
+ }
+ 
+ function index()
+ {
+   $this->load->helper(array('form'));
+   $this->load->view('login');
+ }
+     
+}
+
+?>
---
+++ application/models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author Administrator
+ */
+Class User extends CI_Model
+{
+ function login($username, $password)
+ {
+   $this -> db -> select('emailaddress, password');
+   $this -> db -> from('accounts');
+   $this -> db -> where('emailaddress', $username);
+   $this -> db -> where('password', MD5($password));
+   $this -> db -> limit(1);
+ 
+   $query = $this -> db -> get();
+ 
+   if($query -> num_rows() == 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+ }
+}
+
+?>
